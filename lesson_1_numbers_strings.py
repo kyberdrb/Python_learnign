25de973c013f8b98f0b06b41a8a34368d7f9605b
kyberdrb <kyberdrb@gmail.com>
2017-02-13 23:01:02 +0100

    my first exercise

---
+++ lesson_1_numbers_strings.py
@@ -0,0 +1,65 @@
+Python 3.6.0 (default, Jan 16 2017, 12:12:55) 
+[GCC 6.3.1 20170109] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 8 * 2 * 10
+160
+>>> 8 + 2 * 10
+28
+>>> (8 + 2) * 10
+100
+>>> clear
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    clear
+NameError: name 'clear' is not defined
+>>> 18 / 4
+4.5
+>>> 5 / 4
+1.25
+>>> 4 / 5
+0.8
+>>> 18 / 4
+4.5
+>>> 18 // 4
+4
+>>> 18 % 4
+2
+>>> 5 * 5 * 5
+125
+>>> 5 ** 3
+125
+>>> tuna = 5
+>>> 20 + tuna
+25
+>>> bacon = 18
+>>> bacon / tuna
+3.6
+>>> "wup wup"
+'wup wup'
+>>> 'wup wup'
+'wup wup'
+>>> 'I don't think shes 18'
+SyntaxError: invalid syntax
+>>> 'I don\'t think shes 18'
+"I don't think shes 18"
+>>> 'I don\'t think she\'s 18'
+"I don't think she's 18"
+>>> "I don't think she's 18"
+"I don't think she's 18"
+>>> print()
+
+>>> print("hey now brown cow")
+hey now brown cow
+>>> print("C:\Windows\null")
+C:\Windows
+ull
+>>> print(r"C:\Windows\null")
+C:\Windows\null
+>>> firstName = "Bucky"
+>>> firstName + "Roberts"
+'BuckyRoberts'
+>>> firstName + "W." + "Roberts"
+'BuckyW.Roberts'
+>>> firstName * 5
+'BuckyBuckyBuckyBuckyBucky'
+>>> 
